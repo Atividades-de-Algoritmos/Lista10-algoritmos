09a219b9c53a68db18cf585ac4f195e5482e3fe3
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-12 10:29:50 -0300

    Create ex01.py

---
+++ ex01.py
@@ -0,0 +1,11 @@
+
+
+
+# 1.	 Em linguagem Python, faça um programa que lê uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão é F = (9 * C + 160) / 5, na qual F é a temperatura em Fahrenheit e C é a temperatura em graus Celsius.
+# OBS: o programa de conter três funções:
+# a - Função para ler e retorna o valor da temperatura (não recebe parâmetro)
+# b - Função para fazer o cálculo (recebe como parâmetro a temperatura em graus Celsius)
+# c - Função para mostrar o resultado, recebendo como parâmetro o valor e fazendo a impressão
+
+# funções
+# a
