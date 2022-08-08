a18f65977854002b71ed53b8963763727d71a2ef
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-08-08 05:50:16 -0300

    Função Recursiva 🔄

--- ex03.py
+++ ex03.py
@@ -4,20 +4,24 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 12/07/2022
+# data: 06/08/2022
 #
-# 3. #função recursiva
-# escreva uma função que receba uma string e retorne outra string de forma reversa.
-# # 01-recursiva-reverso
-def reverso(valor): # função recursiva
-    if len(valor) == 1: # condição de parada
-        return valor # retorna o valor da string
+# 3. Escreva uma função que receba uma string e retorne outra string de forma reversa.
+
+# ! função recursiva !
+
+def reverso(valor): # Criando a função reverso recebendo um valor como parâmetro
+    
+    if len(valor) == 1: # Se a string for apenas 1 caractere o programa só retorna o valor que veio
+        return valor # Retorna o valor da string
     else:
-        return reverso(valor[1:]) + valor[0] # chama a função recursiva e faz o cálculo da string reversa
+        return valor[::-1] # Usando o fatiamento conseguimos inverter a string, ele vai percorrer toda a string só que ao contrário e vai retornar o valor invertido.
 
-# programa principal
-valor = input("Digite uma string: ") # lê a string e armazena na variável valor
-print(f"A String original é: {valor}") # faz a impressão da string original
-print(f"O reverso da string é: {reverso(valor)}") # faz a impressão do reverso da string
+# -- Chamada das funções --
 
+valor = input("Digite uma string: ") # Lê a string e armazena na variável valor
 
+print(f"\nA String original é: {valor}") # Faz a impressão da string original
+print(f"O reverso da string é: {reverso(valor)}") # Faz a impressão chamando a função reverso em cima do valor
+
+print('\nFim do programa') # Informando ao usuário que o programa terminou
